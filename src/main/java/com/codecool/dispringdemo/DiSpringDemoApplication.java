4f4f2d160baff705112f18178ddbcc6c5ac6eb14
farkaspeti <farkas.peti97@gmail.com>
2019-08-14 13:27:09 +0200

    Basic Spring demo for practicing Dependency Injection.

---
+++ src/main/java/com/codecool/dispringdemo/DiSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.dispringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiSpringDemoApplication.class, args);
+    }
+
+}
